148d12356b27381312d4bb306d02cb61f5256901
fabian <fabian.maass@gmail.com>
2026-01-02 16:28:56 +0100

    fix: add downloaded status to file-control statusMapping

--- web/src/components/file-control.tsx
+++ web/src/components/file-control.tsx
@@ -188,6 +188,7 @@ export default function FileControl({
       },
     ],
     completed: [removeBtnProps],
+    downloaded: [removeBtnProps],
   };
 
   const actionButtons = file.originalDeleted ? (
